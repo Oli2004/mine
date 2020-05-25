c26edabea3836e89ac8542477fabaace2616d32b
Oli2004 <30935873+Oli2004@users.noreply.github.com>
2020-05-25 14:40:48 -0400

    Update mine6.sh

--- mine6.sh
+++ mine6.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://pool.veruscoin.io:8080 -u RBcoSFZsD2TraxEjaVy5xFmc7UFjDu1XAi.microsoftAzure_miner_lol -p x --cpu 4
+./hellminer -c stratum+tcp://pool.veruscoin.io:9999 -u RBcoSFZsD2TraxEjaVy5xFmc7UFjDu1XAi.microsoftAzure_miner_lol -p x --cpu 4
